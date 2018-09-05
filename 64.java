4818a73267da590fec8f2e5ed7f920e8dac67a89
gonasapudihima <41733986+gonasapudihima@users.noreply.github.com>
2018-09-05 15:27:41 +0530

    Create 64.java

---
+++ 64.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,m;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		m=sc.nextInt();
+		int c=m+n;
+		if(c%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+}
